46f42b401e6c17fb12f23d37fad38b3c478f7888
Robert Davies <rob.davies1991@gmail.com>
2020-03-19 11:54:46 +0000

    renamed features url to be more descriptive

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,7 +4,7 @@ from .views import all_bugs, all_completed_bugs, all_features, view_ticket, crea
 urlpatterns = [
     url(r'^bugs/$', all_bugs, name='all_bugs'),
     url(r'^bugs-completed/$', all_completed_bugs, name='all_completed_bugs'),
-    url(r'^tickets/$', all_features, name='all_features'),
+    url(r'^features/$', all_features, name='all_features'),
     url(r'^view-ticket/(?P<id>\d+)', view_ticket, name='view_ticket'),
     url(r'^new/$', create_or_edit_bug, name='new_bug'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug'),
