bfda8b6c8fbfb504ae13786c301d27743e242b34
Robert Davies <rob.davies1991@gmail.com>
2020-03-29 02:33:49 +0100

    added points value to products and displayed on pages

--- cart/contexts.py
+++ cart/contexts.py
@@ -11,12 +11,14 @@ def cart_contents(request):
 
     cart_items = []
     total = 0
+    points = 0
     product_count = 0
 
     for id, quantity in cart.items():
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.price
+        points += quantity * product.points
         product_count += quantity
         cart_items.append({'id': id, 'quantity': quantity, 'product': product})
 
-    return {'cart_items': cart_items, 'total': total, 'product_count': product_count }
+    return {'cart_items': cart_items, 'total': total, 'points': points, 'product_count': product_count }
--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
+    points = models.IntegerField(blank=False, default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
