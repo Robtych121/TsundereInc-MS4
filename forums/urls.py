c3a904b02a4edb2eadf1c8f2e14610623eaa3b1b
Robert Davies <rob.davies1991@gmail.com>
2020-04-19 13:52:33 +0100

    created view post page and functions

--- forums/urls.py
+++ forums/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
-from .views import forums
+from .views import forums, view_forum, create_or_edit_forum, forum_upvote
 
 urlpatterns = [
-    url(r'^forums/$', forums, name='forums'),
+    url(r'^$', forums, name='forums'),
+    url(r'^view/(?P<id>\d+)', view_forum, name='view_forum'),
+    url(r'^new/$', create_or_edit_forum, name='new_forum'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_forum, name='edit_forum'),
+    url(r'^(?P<pk>\d+)/upvote/$', forum_upvote, name='forum_upvote'),
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import F
+from django.contrib.auth.models import User
 from .models import Forum
+from .forms import ForumForm
 
 # Create your views here.
 def forums(request):
@@ -9,3 +12,50 @@ def forums(request):
     forums = Forum.objects.all()
     username = request.user.username
     return render(request, 'forums.html', {'forums': forums}, username)
+
+def view_forum(request, id):
+    """
+    A view forum page with comments and also increments the views counter
+    """
+    forum = Forum.objects.get(id=id)
+
+    # View Incrementer
+    forumViews = get_object_or_404(Forum, pk=id)
+    forumViews.views = F('views') + 1
+    forumViews.save()
+
+    user = request.user
+    
+    return render(request, 'view_forum.html', {'forum': forum, 'user': user})
+
+def create_or_edit_forum(request, pk=None):
+    """
+    Create a  view that allows us to create
+    or edit a forum post depending if the forum ID
+    is null or not
+    """
+    forum = get_object_or_404(Forum, pk=pk) if pk else None
+    username = request.user.username
+    if request.method == 'POST':
+        form = ForumForm(request.POST, instance=forum)
+        if form.is_valid():
+            forum = form.save(commit=False)
+            forum.author = username.capitalize()
+            forum.save()
+            return redirect(view_forum, forum.pk)
+    else:
+        form = ForumForm(instance=forum)
+    return render(request, 'forumform.html', {'form': form})
+
+def forum_upvote(request, pk=None):
+    """
+    Upvote view for forum
+    """
+    forum = get_object_or_404(Forum, pk=pk) if pk else None
+    user = User.objects.get(email=request.user.email)
+    forum.upvotes = F('upvotes') + 1
+    forum.views = F('views') - 1
+    user.profile.forum_upvotes += 1
+    user.profile.save()
+    forum.save()
+    return redirect(view_forum, forum.pk)
