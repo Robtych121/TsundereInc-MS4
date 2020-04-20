d85eacac3f67ef2804544f5ff2693c8abd800a3f
Robert Davies <rob.davies1991@gmail.com>
2020-04-20 11:18:50 +0100

    created forum comments model

--- forums/models.py
+++ forums/models.py
@@ -14,3 +14,12 @@ class Forum(models.Model):
 
     def __str__(self):
         return '{0} - Author: {1} | Date: {2}'.format(self.name, self.author, self.date)
+
+class Comment(models.Model):
+    content = models.TextField()
+    date = models.DateField(default=datetime.now, blank=True)
+    author = models.CharField(max_length=50, default='')
+    ticketID = models.IntegerField(default=0)
+
+    def __str__(self):
+        return 'Author: {0} | Date: {1} | Content: {2}'.format(self.author, self.date, self.content)
