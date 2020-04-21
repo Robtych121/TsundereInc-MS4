47f8094b7de9ccb9e6e2f03facd423f9d9c93f08
Robert Davies <rob.davies1991@gmail.com>
2020-04-21 13:10:56 +0100

    bugfix for new users not being able to purchase

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,3 +11,9 @@ class Profile(models.Model):
     forum_upvotes = models.IntegerField(blank=False, default=0)
     points_available = models.IntegerField(blank=False, default=0)
     cash_used = models.IntegerField(blank=False, default=0) 
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
